34b87b738b6dbba7c5b34e171abedc4710c80f0b
Eric <ci_re@foxmail.com>
2021-12-25 16:20:33 +1100

    feat(components): added 404 page and matched routing

---
+++ src/pages/404.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+'404'
+
+import PageBlock from '@c/Layout/PageBlock.vue'
+</script>
+
+<template>
+  <div></div>
+  <page-block>
+    <h1 class="col-span-8">404 Page not found.</h1>
+  </page-block>
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 
-const routes = [
+const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     redirect: { name: 'Home' },
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: '404',
+    component: () => import('@p/404.vue'),
+    meta: { title: '404 - Page not found | FGL - Design System' },
   },
   {
     path: '/home',
@@ -12,31 +18,37 @@ const routes = [
     meta: { title: 'Home | FGL - Design System' },
   },
   {
-    path: '/color',
+    path: '/visual-color',
     name: 'Color',
     component: () => import('@p/Visual/VisualColor.vue'),
     meta: { title: 'Color | FGL - Design System' },
   },
+
   {
-    path: '/typography',
+    path: '/visual-typography',
     name: 'Typography',
     component: () => import('@p/Visual/VisualTypography.vue'),
     meta: { title: 'Typography | FGL - Design System' },
   },
   {
-    path: '/spacing',
+    path: '/visual-spacing',
     name: 'Spacing',
     component: () => import('@p/Visual/VisualSpacing.vue'),
     meta: { title: 'Spacing | FGL - Design System' },
   },
   {
-    path: '/breakpoints',
+    path: '/visual-breakpoints',
     name: 'Breakpoints',
     component: () => import('@p/Visual/VisualBreakpoints.vue'),
     meta: { title: 'Breakpoints | FGL - Design System' },
   },
+  {
+    path: '/element-articles',
+    name: 'ElementArticle',
+    component: () => import('@p/Elements/ElementArticle.vue'),
+    meta: { title: 'Articles | FGL - Design System' },
+  },
 ]
-
 const router = createRouter({
   history: createWebHistory(),
   routes,
